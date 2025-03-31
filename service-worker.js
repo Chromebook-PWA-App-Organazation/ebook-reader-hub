cee632f014e368817b259f372ee78893c677083f
Sebby Webby <120981002+testing-platform-games@users.noreply.github.com>
2025-03-31 09:04:23 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,10 @@ const ASSETS_TO_CACHE = [
     "./pdf.html",
     "./manifest.json",
     "./icons/icon-192x192.png",
-    "./icons/icon-512x512.png"
+    "./icons/icon-512x512.png",
+    "./preview/preview-1.png",  // Added preview images
+    "./preview/preview-2.png",  // Added preview images
+    "./preview/preview-3.png"   // Added preview images
 ];
 
 // Install Service Worker and Cache Assets
@@ -16,27 +19,6 @@ self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             return cache.addAll(ASSETS_TO_CACHE);
-        })
-    );
-});
-
-// Activate Service Worker and Clean Old Caches
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            );
-        })
-    );
-});
-
-// Fetch Event - Serve Cached Files First
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
         })
     );
 });
